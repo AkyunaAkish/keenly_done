b363caba211a81b06a25efe888e62cc6bfbb2f1f
AkyunaAkish <akyunaakish@gmail.com>
2015-08-28 00:16:50 -0600

    added server

---
+++ app.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+
+var cors = require('cors')
+app.use(cors())
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(__dirname + '/public'));
+
+
+app.use(function(req, res) {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+var port = process.env.PORT || 3000;
+
+
+app.listen(process.env.PORT || 3000, function(){
+   console.log('listening');
+});
+
+
+module.exports = app;
