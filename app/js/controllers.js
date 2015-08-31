97554dfaaf66098d0b4a0b1d98e84851ca0293f9
AkyunaAkish <akyunaakish@gmail.com>
2015-08-31 11:58:42 -0600

    added stuff

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -10,7 +10,7 @@ app.controller('AuthController', function($scope, $location, $firebaseAuth){
 
   $scope.login = function(){
     authObj.$authWithPassword($scope.user).then(function(){
-      $location.path('/#/main');
+      $location.path('/main');
     },function(){
       alert('Invalid email or password');
     })
@@ -28,32 +28,46 @@ app.controller('PostsController', function($scope, $firebaseArray, $firebaseAuth
   var postsRef = new Firebase("https://keenlydone.firebaseio.com/posts");
   //use reference to create synchronized array
   $scope.posts = $firebaseArray(postsRef);
-  $scope.newPost = {text: "", completed: false};
+  $scope.newPost = {title: "", author: "", description: "", imageUrl: "", videoUrl: "", audioUrl: ""};
 
   $scope.addPost = function(){
     $scope.posts.$add($scope.newPost).then(function(data){
-      $scope.newPost.text = "";
+      $scope.newPost.title = "";
+      $scope.newPost.author = "";
+      $scope.newPost.description = "";
+      $scope.newPost.imageUrl = "";
+      $scope.newPost.videoUrl = "";
+      $scope.newPost.audioUrl = "";
     })
   }
 
   $scope.showPost = function(post){
-    $scope.text = todo.text;
-    $scope.id = todo.$id;
+    $scope.title = post.title;
+    $scope.author = post.author;
+    $scope.description = post.description;
+    $scope.imageUrl = post.imageUrl;
+    $scope.videoUrl = post.videoUrl;
+    $scope.audioUrl = post.audioUrl;
+    $scope.id = post.$id;
   }
 
-  $scope.saveChanges = function(post,text,completed){
+  $scope.saveChanges = function(post,title,author,description,imageUrl,videoUrl,audioUrl){
     //var id = $scope.id;
     //var record = $scope.todos.$getRecord(id);
     console.log(post);
-    console.log(text);
-    console.log(completed);
-    post.text = text;
-    if (completed == 'completed') {
-      post.completed = true;
-    } else {
-      post.completed = false;
-    }
+    console.log(title);
+    console.log(author);
+    console.log(description);
+    console.log(imageUrl);
+    console.log(videoUrl);
+    console.log(audioUrl);
 
+    post.title = title;
+    post.author = author;
+    post.description = description;
+    post.imageUrl = imageUrl;
+    post.videoUrl = videoUrl;
+    post.audioUrl = audioUrl;
     console.log(post);
 
     $scope.posts.$save(post);
